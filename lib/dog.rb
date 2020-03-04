ee96830e90ba91b106533a9376f8d66499482faf
Ryna Truong <rynatruong@outlook.com>
2020-03-04 04:14:04 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+class Dog 
+  @@all = []
+  
+  attr_accessor (:name)
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all = []
+  end
+  
+  def self.print_all
+    @@all.map do |dog|
+      puts dog.name
+    end
+  end
+end
+
